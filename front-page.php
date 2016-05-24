b55884617b504c04f1f8b0cb9158f2a08b0ef377
Tim Booher <ads@theboohers.org>
2016-05-24 02:32:03 +0000

    updated home page

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
 	<div class="row margin-top-bottom">
 		<div class="col-sm-4 margin-bottom">
 			<?php
-                $args = array( 'post_type' => 'wod', 'posts_per_page' => 1 );
+                $args = array( 'post_type' => 'post', 'posts_per_page' => 1 );
                 $loop = new WP_Query( $args );
                 while ( $loop->have_posts() ) :
                   $loop->the_post();
@@ -20,8 +20,9 @@
                     } else {
                         $comments = __('1 Comment');
                     }
-                    $write_comments = '<a href="' . get_comments_link() .'">'. $comments.'</a>';
-                  $image = get_the_post_thumbnail(get_the_ID(), 'medium', array( 'class' => 'img-thumbnail img-responsive' ));
+                  $write_comments = '<a href="' . get_comments_link() .'">'. $comments.'</a>';
+                  $the_id = get_the_ID();  
+                  $image = get_the_post_thumbnail($the_id, 'medium', array( 'class' => 'img-thumbnail img-responsive' ));
                 endwhile;
                 wp_reset_postdata();
                 if ($image == '') {
@@ -31,13 +32,11 @@
             <?php echo $image; ?>
 		</div>
 		<div class="col-sm-8 margin-bottom">
-			<?php   
-			$my_id = 10866;
-			$post_id_5369 = get_post($my_id);
-			$content = $post_id_5369->post_content;
-			$content = apply_filters('the_content', $content);
-			$content = str_replace(']]>', ']]>', $content);
-			echo $content;
+			<?php
+			  if (have_posts()): while (have_posts()): the_post();
+			  	wp_title(''); echo '<br />';
+				the_content(); echo '<br />';
+			  endwhile; endif;
 			?>
 		</div>
 	</div>
@@ -48,31 +47,14 @@
 <div class="row">
 	<div class="wod col-sm-6">
 		<div class="card ">
-		<div class="cardtitle">
-			<h3>today's WOD</h3>
-		</div>
-		<?php
-		
-		$args = array( 'posts_per_page' => 1 );
-		
-		// The Query
-		$the_query = new WP_Query( $args );
-		
-		// The Loop
-		if ( $the_query->have_posts() ) {
-			echo '<ul>';
-			while ( $the_query->have_posts() ) {
-				$the_query->the_post();
-				echo '<li><h4>' . the_title() . '</h4></li>';
-				echo '<p>' . the_content() . '</p>';
-			}
-			echo '</ul>';
-		} else {
-			// no posts found
-			echo 'nothing!';
-		}
-		
-		?>
+			<div class="cardtitle">
+				<h3>today's WOD</h3>
+			</div>
+			<h4><a href="<?php echo $permalink ?>"><?php echo $title; ?></a></h4>
+			<div class="entry-content">
+				<?php echo apply_filters( 'the_content', $content ); ?>
+				<p><?php echo $write_comments; ?></p>
+			</div>	
 		</div><!-- .card -->
 	</div><!-- .wod -->
 	<div class="news col-sm-6">
